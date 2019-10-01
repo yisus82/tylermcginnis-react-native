979bcc7710786c534cabe7444be418762d5abfae
yisus82 <djalma8@gmail.com>
2019-10-01 17:19:06 +0200

    Granted Geolocation

--- UdaciFitness/components/Live.js
+++ UdaciFitness/components/Live.js
@@ -32,12 +32,45 @@ const styles = StyleSheet.create({
     color: white,
     fontSize: 20,
   },
+  directionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: 'center',
+  },
+  metricContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: purple,
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 5,
+  },
 });
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'denied',
+    status: 'granted',
     direction: '',
   };
 
@@ -76,8 +109,20 @@ export default class Live extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>North</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+          </View>
+        </View>
       </View>
     );
   };
